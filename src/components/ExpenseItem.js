4fe29a566d0520b281ae2180da5766e15f9ae3a2
Leon Zheng <leon.zheng@hotmail.com>
2022-05-11 15:29:31 +1000

    seperated ExpenseDate

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,10 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-  const year = props.date.toLocaleString("en-US", { month: "long" });
-  const month = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const day = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>
-        <div>{year}</div>
-        <div>{month}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
