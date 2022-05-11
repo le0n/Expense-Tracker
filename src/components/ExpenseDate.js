64e8d52fdf3bebc4356b4c3d48c6e9be7af0e27d
Leon Zheng <leon.zheng@hotmail.com>
2022-05-11 17:12:01 +1000

    asd

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,9 +1,9 @@
 import "./ExpenseDate.css";
 
 const ExpenseDate = (props) => {
-  const year = props.date.toLocaleString("en-US", { month: "long" });
-  const month = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const day = props.date.getFullYear();
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
     <div className="expense-date">
